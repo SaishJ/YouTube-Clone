847a68691dabdb1b73b29cbd2818ab86f2813287
SaishJ <saishjagtap0003@gmail.com>
2023-01-24 20:06:54 +0530

    :hammer: Building Video component
    Building Video component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channeId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
