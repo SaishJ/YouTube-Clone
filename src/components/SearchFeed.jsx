e516968f40d24970b1552b499c0fd0d44fc4e7c3
SaishJ <saishjagtap0003@gmail.com>
2023-02-02 12:39:45 +0530

    :white_check_mark: Building SearchFeed
    Building SearchFeed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
 
-const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+const Feed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Result for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+
+      <Videos videos={videos} justifyContent="center" />
+    </Box>
+  );
 };
 
-export default SearchFeed;
+export default Feed;
